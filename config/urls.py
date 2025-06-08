3fab910d5db43449ec35c15ce94322f9c3d9ae25
Ilia Ryutin <iryutin@mail.ru>
2025-06-08 19:27:25 +0300

    вроде готово

---
+++ config/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('mailings.urls', namespace='mailings')),
+    path('', include('mailings_service.urls', namespace='blog')),
+    path('', include('user.urls', namespace = 'user'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
